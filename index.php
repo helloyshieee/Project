aeb8a888ee3341da5b5bffb492265a36269f4df2
Aira Ashley <aashley.villalobos@outlook.com>
2022-03-24 00:58:00 -0500

    Added a header

---
+++ index.php
@@ -0,0 +1,64 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>A.V. Collection</title>
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="preconnect" href="https://fonts.googleapis.com">
+		<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+		<link href="https://fonts.googleapis.com/css2?family=Heebo&family=Montserrat:wght@500&family=Open+Sans:wght@600&family=Roboto:wght@300;400&display=swap" rel="stylesheet">
+	</head>
+	<body>
+		<!-- <a href =register.html> Register </a>
+        <a href =login.html> Log In </a> -->
+		<div class="announcement">
+			<p>FREE SHIPPING ON ORDERS CAD$200 (CANADA & US) </p>
+		</div>
+
+		<div class="header">
+			<div class = "header-text">
+			<h1>A.V. COLLECTION</h1>
+    		<p>Hello there!</p>
+    		<button>Shop Now</button>
+			</div>
+		</div>
+
+		<div class = "vision">
+			<div class = "blocks">
+				<h2 class = "custom-title">Minimal</h2>
+			</div>
+			<div class = "custom-text">
+				<p>We are all about the simplicity. 
+				It makes our designs easy to use while still keeping the aesthetic that you love.</p>
+			</div>
+		</div>
+
+		<div class = "container">
+		<div class = "vision">
+			<div class = "blocks">
+				<h2 class = "custom-title">Functional</h2>
+			</div>
+			<div class = "custom-text">
+				<p>Our planners are not just pretty, but meant to be used. 
+				They are created to inspire you to be more productive and organized.</p>
+			</div>
+		</div>
+		
+		<div class = "vision">
+			<div class = "blocks">
+				<h2 class = "custom-title">Personalized</h2>
+			</div>
+			<div class = "custom-text">
+				<p>Let's create your dream planner together. 
+					Pitch us your ideas and we'll work our magic to help you revamp your planner with style.</p>
+			</div>
+		</div>
+		</div>
+
+		
+	</body>
+</html>
