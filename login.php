50c5e362e2af5f23b6efac2f1bd6b547033f212d
Aira Ashley <aashley.villalobos@outlook.com>
2022-03-24 22:54:34 -0500

    Added a username and password verification

--- login.php
+++ login.php
@@ -2,25 +2,10 @@
 session_start();
 require("connect.php");
 
-$message = " ";
-if(isset($_GET['message'])){
-    $message = $_GET['message'];
-}
+$msg = ""; 
 
-
-// if(isset($_POST['btnlogin'])){
-
-    // $username = $_POST['username'];
-    // $passwordAttempt = $_POST['password'];
-
-    // $sql = "SELECT userId, username, password FROM users WHERE username = :username";
-    // $stmt = $db->prepare($sql);
-    // $stmt->bindValue('username', $username);
-    // $stmt->execute();
-    // $user = $stmt->fetch(PDO::FETCH_ASSOC);
-
-    $msg = ""; 
 if(isset($_POST['btnlogin'])) {
+  $success = 0;
   $username = trim($_POST['username']);
   $password = trim($_POST['password']);
   if($username != "" && $password != "") {
@@ -32,15 +17,24 @@ if(isset($_POST['btnlogin'])) {
       $stmt->execute();
       $count = $stmt->rowCount();
       $row   = $stmt->fetch(PDO::FETCH_ASSOC);
+
+  
       if($count == 1 && !empty($row)) {
-        /******************** Your code ***********************/
-        $_SESSION['sess_user_id']   = $row['userId'];
-        $_SESSION['sess_user_name'] = $row['username'];
-        // $_SESSION['sess_name'] = $row['name'];
-        header('Location: home.php');
-                exit;
-      } else {
-        $msg = "Invalid username and password!";
+
+          
+            $_SESSION['sess_user_id']   = $row['userId'];
+            $_SESSION['sess_user_name'] = $row['username'];
+            header('Location: home.php');
+                    exit;
+            $success =1;
+          
+      } 
+      if($success == 0)
+      {
+        echo'Invalid username or Password';
+      }
+      else{
+        echo'Test';
       }
     } catch (PDOException $e) {
       echo "Error : ".$e->getMessage();
@@ -48,38 +42,5 @@ if(isset($_POST['btnlogin'])) {
   } else {
     $msg = "Both fields are required!";
   }
-
-
-
-
-
-
-    // $_SESSION['user_id'] = $user['userId'];
-    // $_SESSION['logged_in'] = time();
-
-    // if(!isset($user)){
-    //     die('Incorrect username / password combination!');
-    // } else{
-    //     echo'User is set';
-
-    //     $validPassword = password_verify($passwordAttempt, $user['password']);
-    //     echo  $validPassword;
-    //     if($validPassword){
-
-    //         $_SESSION['user_id'] = $user['userId'];
-    //         $_SESSION['logged_in'] = time();
-
-    //         header('Location: home.php');
-    //         exit;
-
-    //     } elseif ($validPassword == null) {
-    //         echo'password is null';
-    //     }
-    //     else{
-    //         $message = 'Incorrect username / password!';
-    //     }
-    // }
 }
-
-echo $_SESSION['user_id'];
 ?>
